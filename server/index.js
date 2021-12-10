8e3a4938d9ce8f3fadf0707e5d0869b1ce84f3fa
Julian Sharon <julianshrn@gmail.com>
2021-12-10 18:46:34 +0530

    added dynamic query

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 const app = express();
 const request = require("request-promise");
 const { html } = require("cheerio");
@@ -9,12 +10,23 @@ app.listen(3000, () => {
   console.log("server is up and running");
 });
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.send("Welcome to image scrapper API");
 });
-app.get("/meme", (req, res) => {
+
+var jsonParser = bodyParser.json();
+app.post("/urls", jsonParser, (req, res) => {
+  console.log(req);
+  const body = req.body;
+  console.log(body);
+  var query = body["query"];
+  query = query.trim().replaceAll("", "+");
+
   var data;
   request({
-    uri: "https://www.google.com/search?q=meme+templates&hl=en-US&source=lnms&tbm=isch",
+    uri:
+      "https://www.google.com/search?q=" +
+      query +
+      "&hl=en-US&source=lnms&tbm=isch",
     header: {
       "accept-encoding": "gzip,deflate,br",
       "accept-language": "en-US,en;q=0.9",
@@ -22,7 +34,7 @@ app.get("/meme", (req, res) => {
     gzip: true,
   })
     .then((html) => {
-      res.send(html);
+      //res.send(html);
       data = html;
       $ = cheerio.load(html);
       const allChildren = $("img");
@@ -31,33 +43,7 @@ app.get("/meme", (req, res) => {
         console.log($(image).attr("src"));
         resImageList.push($(image).attr("src"));
       });
-      //res.send(resImageList);
+      res.send(resImageList);
     })
     .catch((err) => console.log("errrrrr" + err));
 });
-
-async function scrapper() {}
-
-function getPropertyString(obj) {
-  var res = "";
-  var stack = [];
-  var objectHistory = []; // added this
-  stack.push(obj);
-  while (stack.length > 0) {
-    var object = stack.pop();
-    if (objectHistory.indexOf(object) != -1) continue; // added this
-    objectHistory.push(object); // added this
-    res += "[";
-    for (var prop in object) {
-      if (prop == null) continue;
-      if (typeof object[prop] === "object") {
-        stack.push(object[prop]);
-      } else {
-        res +=
-          prop + ": " + object[prop].toString().replace(/[\t\r\n]/g, "") + ", ";
-      }
-    }
-    res += "],";
-  }
-  return res;
-}
